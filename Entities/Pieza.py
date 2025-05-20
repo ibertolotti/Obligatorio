51fa09d7b0f1bc79ed984db7ea3ced0157f1cb9c
ibertolotti <ibertolotti@correo.um.edu.uy>
2025-05-20 10:22:36 -0300

    Update Pieza.py
    empezamos pieza

--- Entities/Pieza.py
+++ Entities/Pieza.py
@@ -1,7 +1,10 @@
 class Pieza:
+    contador_codigos=1
     def __init__(self,codigo, descripcion, costo_USD, lote, cantidad_stock):
-        self.codigo =
-        self.descripcion =
-        self.costo_USD =
-        self.lote =
-        self.cantidad_stock = 
+        contador_codigos=contador_codigos+1
+        
+        self.codigo = int(codigo)
+        self.descripcion = descripcion
+        self.costo_USD = float(costo_USD)
+        self.lote = int(lote)
+        self.cantidad_stock = int(cantidad_stock)
