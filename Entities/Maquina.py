2eb0135717248121ab8d5c9e3e0455a536f5b6d7
ibertolotti <ibertolotti@correo.um.edu.uy>
2025-05-23 09:09:43 -0300

    avance clase

---
+++ Entities/Maquina.py
@@ -0,0 +1,12 @@
+from requerimiento import Requerimiento
+
+class Maquina:
+    codigo_maquina = 0
+    def __init__(self, descripcion):
+        Maquina.codigo_maquina += 1
+
+        self.codigo = Maquina.codigo_maquina
+        self.descripcion = descripcion
+    
+    def costo_produccion(self):
+        return 
