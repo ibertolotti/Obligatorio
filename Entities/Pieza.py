535b3d1c8e54bedb08b99de9474b3aaa934ee282
Josefina Vazquez <jvazquez1@correo.um.edu.uy>
2025-05-23 07:54:44 -0300

    Update Pieza.py

--- Entities/Pieza.py
+++ Entities/Pieza.py
@@ -1,7 +1,7 @@
 class Pieza:
     def __init__(self,codigo, descripcion, costo_USD, lote, cantidad_stock):
-        self.codigo =
-        self.descripcion =
+        self.codigo = 
+        self.descripcion = 
         self.costo_USD =
         self.lote =
         self.cantidad_stock = 
