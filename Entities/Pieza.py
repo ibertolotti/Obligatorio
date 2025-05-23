acd2deee333709f9b5d6deaebc829f7e62c0a9dd
Josefina Vazquez <jvazquez1@correo.um.edu.uy>
2025-05-23 08:13:42 -0300

    Cambios

--- Entities/Pieza.py
+++ Entities/Pieza.py
@@ -1,13 +1,7 @@
 class Pieza:
     contador_codigos=1
     def __init__(self,codigo, descripcion, costo_USD, lote, cantidad_stock):
-<<<<<<< HEAD
-        self.codigo = 
-        self.descripcion = 
-        self.costo_USD =
-        self.lote =
-        self.cantidad_stock = 
-=======
+        
         contador_codigos=contador_codigos+1
         
         self.codigo = int(codigo)
@@ -15,4 +9,3 @@ class Pieza:
         self.costo_USD = float(costo_USD)
         self.lote = int(lote)
         self.cantidad_stock = int(cantidad_stock)
->>>>>>> ebed3d25c6dbb1b7ea740c0d217b42825ba5631b
