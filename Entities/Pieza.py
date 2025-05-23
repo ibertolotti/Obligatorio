91ae049dc8dafe3deead7c67b3476e208d764f61
luciarippe <luciarippe@gmail.com>
2025-05-23 09:37:35 -0300

    Update Pieza.py
    property en pieza

--- Entities/Pieza.py
+++ Entities/Pieza.py
@@ -3,8 +3,20 @@ class Pieza:
     def __init__(self, descripcion, costo_USD, lote, cantidad_stock=0):
         Pieza.codigo_pieza += 1
         
-        self.codigo = Pieza.codigo_pieza
-        self.descripcion = descripcion
-        self.costo_USD = float(costo_USD)
-        self.lote = int(lote)
-        self.cantidad_stock = int(cantidad_stock)
+        self.__codigo = Pieza.codigo_pieza
+        self.__descripcion = descripcion
+        self.__costo_USD = float(costo_USD)
+        self.__lote = int(lote)
+        self.__cantidad_stock = int(cantidad_stock)
+
+    @property
+    def codigo(self):
+        return self.__codigo
+    
+    @property
+    def descripcion(self):
+        return self.__descripcion
+    
+    @property
+    def costo_USD(self):
+        return self.__costo_USD
